8bb6b732be2841961a40af70e5fc4cbbd57a398a
Gustavo Junkes <87446204+gustavoJunkes@users.noreply.github.com>
2023-03-23 10:29:07 -0300

    Update enviroment-java.sh

--- enviroment-java.sh
+++ enviroment-java.sh
@@ -1,23 +1,24 @@
 #! /usr/bin/bash
 
-# Install java and config java
+# Install and config java
 
-#sudo apt update
+sudo apt update
 
-sudo apt-get update
+sudo apt upgrade
 
 JAVA_VER=$(java -version 2>&1 >/dev/null | egrep "\S+\s+version" | awk '{print $3}' | tr -d '"')
 
 JAVA_PATH=$(which java)
 
-if [ -z "$JAVA_VER" ] && [ -z $JAVA_PATH ]  
+if [ -z "$JAVA_VER" ] && [ -z "$JAVA_PATH" ]  
 then
     echo "Java não está instalado"
-    echo "Instalando java..."
+    echo "Instalando JDK 11..."
     
-    sudo apt install default-jdk
+    sudo apt -y install default-jdk
 
-    # Set the java home
+    # Set the java home --later
+    echo `update-alternatives --config java`
 else
-    echo "Java instalation was found"
+    echo "A Java instalation was found"
 fi
